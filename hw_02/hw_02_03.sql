beff72a62e62962617af9cc02dc74bf25eabb92c
TomTheCat <richest@bk.ru>
2019-08-10 17:53:49 +0300

    Тоже изначально загрузился какой-то не тот файл, отправляю актуальный

--- hw_02/hw_02_03.sql
+++ hw_02/hw_02_03.sql
@@ -1,6 +1,27 @@
---
+-- создал Базу Данных (см. скриншоты по 1му заданию)
 
+-- открыл файл .my.cnf и вставил [client] вместо [mysql]
 
-/var/lib/mysql/example/
+-- В терминале сохранил (сделал дамп) созданную на предыдущем этапе БД example 
+mysqldump example > example_backup.sql
 
-cd /usr/local/var/mysql/
+-- Зашёл в mysql
+CREATE DATABASE sample;
+USE sample;
+SOURCE example_backup.sql;
+
+-- проверил что новая таблица похожа на "старую", с которой сделан dump
+SHOW TABLES;
+DESCRIBE users;
+SELECT * FROM users;
+
+-- Попробовал другим способом развернуть БД из дампа, зашёл в mysql:
+CREATE DATABASE sample2;
+
+-- Затем в баше:
+mysql sample2 < example_backup.sql
+
+-- проверил что новая таблица похожа на "старую", с которой сделан dump
+SHOW TABLES;
+DESCRIBE users;
+SELECT * FROM users;
